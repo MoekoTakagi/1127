54aa024a996c2d2e95fd21417ffa002b11a49bec
Moeko Takagi <barbiepinkmindoll@gmail.com>
2016-01-20 01:11:05 +0900

    [Completion]飛車の配置(Placement of Rook)DFS

--- rook.c
+++ rook.c
@@ -9,26 +9,22 @@
 
 入力で升目nを受け取り、可能な配置の総数を出力する。
 再帰を用いて深さ優先探索を行う。
-
-b.1/17 自分で作成。
-c. 全ての入力に対して成り立つ。
 */
 
 #include <stdio.h>
-#include <string.h>
 #include <stdbool.h>
 
-#define MAX 100 //jが置かれているかどうか
+#define MAX 100
 
 int depth_first_search(int i); //深さ優先探索
-void set_dragon(int i, int j); //竜を置く
-void remove_dragon(int i, int j); //竜を除く
-int is_this_correct(); //正解かどうか
+void set_dragon(int i, int j); //飛車を置く
+void remove_dragon(int i, int j); //飛車を除く
+bool promoted_dragon(); //当たりの駒がなければ竜に成る
 void print_state(); //現在の駒の状態を表示する
 
 static int n;
-static int isj[MAX] = {0};
 static int counter = 0;
+static bool isj[MAX];
 static bool node[MAX];
 
 int main(){
@@ -38,18 +34,13 @@ int main(){
 }
 
 int depth_first_search(int i){
-	if(i >= n){
-	//	print_state();
-		if(is_this_correct() == 1) counter++; //1なら正解、0なら不正解
-		return 1;
-	}
-
 	int j = 0;
+	if(i >= n && promoted_dragon()){counter++; return 1;}
 	while(j < n){
-		if(isj[j] == 0){ //j列に竜が置かれていなければ
-			set_dragon(i, j); //(i, j)に竜を置く
+		if(!isj[j]){ //j列に竜が置かれていなければ
+			set_dragon(i, j); //(i, j)に飛車を置く
 			depth_first_search(i+1); //次の行で探索
-			remove_dragon(i, j); //(i, j)の竜を除く
+			remove_dragon(i, j); //(i, j)の飛車を除く
 		}
 		j++;
 	}
@@ -58,29 +49,29 @@ int depth_first_search(int i){
 
 void set_dragon(int i, int j){
 	node[i*n+j] = true;
-	isj[j] = 1;
+	isj[j] = true;
 }
 
 void remove_dragon(int i, int j){
 	node[i*n+j] = false;
-	isj[j] = 0;
+	isj[j] = false;
 }
 
-int is_this_correct(){
+bool promoted_dragon(){
 	int i, j, k;
 	for(j = 0; j < n; j++){
 		for(i = 0; i < n; i++){
 			k = i*n+j; //添字
-			if(j == 0 && node[k] == true){ //一番左の列に関して、右上・右下に竜があるか
-				if(node[k-n+1] == true || node[k+n+1] == true) return 0;
-			}else if(j == n-1 && node[k] == true){ //一番右の列に関して、左上・左下に竜があるか
-				if(node[k-n-1] == true || node[k+n-1] == true) return 0;
-			}else if(node[k] == true){ //それ以外の列に関して、右上・右下・左上・左下に竜があるか
-				if(node[k-n-1] == true || node[k+n-1] == true || node[k-n+1] == true || node[k+n+1] == true) return 0;
+			if(j == 0 && node[k]){ //一番左の列に関して、右上・右下に竜があるか
+				if(node[k-n+1] || node[k+n+1]) return false;
+			}else if(j == n-1 && node[k]){ //一番右の列に関して、左上・左下に竜があるか
+				if(node[k-n-1] || node[k+n-1]) return false;
+			}else if(node[k]){ //それ以外の列に関して、右上・右下・左上・左下に竜があるか
+				if(node[k-n-1] || node[k+n-1] || node[k-n+1] || node[k+n+1]) return false;
 			}
 		}
 	}
-	return 1; //途中で戻らなければ正解
+	return true; //途中で戻らなければ竜に成る
 }
 
 void print_state(){
